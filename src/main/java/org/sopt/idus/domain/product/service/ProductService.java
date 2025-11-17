69c343d09a254a7f2c6b82d020eb40689cfa602f
dlwjddus1112 <dlwjddus1112@naver.com>
2025-11-17 21:14:01 +0900

    [Feat] #11 좋아요 생성 비즈니스 로직 작성

--- src/main/java/org/sopt/idus/domain/product/service/ProductService.java
+++ src/main/java/org/sopt/idus/domain/product/service/ProductService.java
@@ -7,8 +7,13 @@ import org.sopt.idus.domain.product.entity.Product;
 import org.sopt.idus.domain.product.repository.ProductRepository;
 import org.sopt.idus.domain.productimage.entity.ProductImage;
 import org.sopt.idus.domain.productimage.repository.ProductImageRepository;
+import org.sopt.idus.domain.productlike.entity.ProductLike;
+import org.sopt.idus.domain.productlike.repository.ProductLikeRepository;
 import org.sopt.idus.domain.review.entity.Review;
 import org.sopt.idus.domain.review.repository.ReviewRepository;
+import org.sopt.idus.domain.user.entity.User;
+import org.sopt.idus.domain.user.repository.UserRepository;
+import org.sopt.idus.domain.user.service.UserService;
 import org.sopt.idus.global.exception.customexception.CustomException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,6 +33,12 @@ public class ProductService {
 
     private final ReviewRepository reviewRepository;
 
+    private final UserRepository userRepository;
+
+    private final UserService userService;
+
+    private final ProductLikeRepository productLikeRepository;
+
     public ProductResponse getProductDetail(Long productId){
         Product product = findById(productId);
 
@@ -45,6 +56,18 @@ public class ProductService {
 
     private Product findById(Long productId){
         return productRepository.findById(productId).orElseThrow(() -> new CustomException(PRODUCT_NOT_FOUND));
+    }
+
+    @Transactional
+    public void createProductLike(Long productId, Long userId) {
+        Product product = findById(productId);
+        User user = userService.findById(userId);
+
+        ProductLike productLike = ProductLike.create(product, user);
+        productLikeRepository.save(productLike);
+
+        productRepository.increaseLikeCount(product);
+
     }
 
 }
