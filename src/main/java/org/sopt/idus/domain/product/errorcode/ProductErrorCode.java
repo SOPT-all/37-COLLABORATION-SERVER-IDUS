c4fa64f8e75e06d6862aa313d9823f101aef65b3
dlwjddus1112 <dlwjddus1112@naver.com>
2025-11-17 19:46:30 +0900

    [Feat] #9 작품 관련 ErrorCode 상수 클래스 작성

---
+++ src/main/java/org/sopt/idus/domain/product/errorcode/ProductErrorCode.java
@@ -0,0 +1,25 @@
+package org.sopt.idus.domain.product.errorcode;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.idus.global.exception.errorcode.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum ProductErrorCode implements ErrorCode {
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "존재하지 않는 작품입니다."),
+    ;
+
+
+    private final int httpStatus;
+
+    private final String message;
+    @Override
+    public int getHttpStatus() {
+        return 0;
+    }
+
+    @Override
+    public String getMessage() {
+        return "";
+    }
+}
