c69a7acfb5df165ec06cba8018b45b3d51f41f3d
dlwjddus1112 <dlwjddus1112@naver.com>
2025-11-17 20:31:20 +0900

    [Feat] #9 작품 조회 시 평균 별점도 반환

--- src/main/java/org/sopt/idus/domain/product/service/ProductService.java
+++ src/main/java/org/sopt/idus/domain/product/service/ProductService.java
@@ -7,6 +7,8 @@ import org.sopt.idus.domain.product.entity.Product;
 import org.sopt.idus.domain.product.repository.ProductRepository;
 import org.sopt.idus.domain.productimage.entity.ProductImage;
 import org.sopt.idus.domain.productimage.repository.ProductImageRepository;
+import org.sopt.idus.domain.review.entity.Review;
+import org.sopt.idus.domain.review.repository.ReviewRepository;
 import org.sopt.idus.global.exception.customexception.CustomException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,10 +26,21 @@ public class ProductService {
 
     private final ProductImageRepository productImageRepository;
 
+    private final ReviewRepository reviewRepository;
+
     public ProductResponse getProductDetail(Long productId){
         Product product = findById(productId);
+
         List<ProductImage> productImages = productImageRepository.findAllByProductId(productId);
-        return ProductResponse.from(product, productImages);
+
+        List<Review> reviews = reviewRepository.findAllByProductId(productId);
+
+        double averageScore = reviews.stream()
+                .mapToInt(Review::getScore)
+                .average()
+                .orElse(0.0);
+
+        return ProductResponse.from(product, productImages, averageScore);
     }
 
     private Product findById(Long productId){
