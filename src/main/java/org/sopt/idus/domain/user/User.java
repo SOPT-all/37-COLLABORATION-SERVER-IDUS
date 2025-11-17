409fafc4a92a72e8c4cfdd74ababec533d708df8
dietken1 <dietken1@ajou.ac.kr>
2025-11-17 15:05:04 +0900

    [Feat] #5 User 엔티티 추가

---
+++ src/main/java/org/sopt/idus/domain/user/User.java
@@ -0,0 +1,28 @@
+package org.sopt.idus.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickname;
+
+    private String profileImageUrl;
+
+    @Builder
+    public User(String nickname, String profileImageUrl) {
+        this.nickname = nickname;
+        this.profileImageUrl = profileImageUrl;
+    }
+}
