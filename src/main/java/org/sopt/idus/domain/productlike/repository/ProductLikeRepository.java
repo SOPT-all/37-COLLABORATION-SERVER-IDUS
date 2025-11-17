f014ae28d2dd72f0f40209a80fcf9a557be36686
dlwjddus1112 <dlwjddus1112@naver.com>
2025-11-17 21:13:44 +0900

    [Feat] #11 좋아요 생성 시 좋아요 수 증가하도록 증분 쿼리 작성

---
+++ src/main/java/org/sopt/idus/domain/productlike/repository/ProductLikeRepository.java
@@ -0,0 +1,7 @@
+package org.sopt.idus.domain.productlike.repository;
+
+import org.sopt.idus.domain.productlike.entity.ProductLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductLikeRepository extends JpaRepository<ProductLike, Long> {
+}
