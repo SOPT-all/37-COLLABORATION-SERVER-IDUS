9e96d37300db28da8f7f5ab7f3ea6a731b2ef3a1
dietken1 <dietken1@ajou.ac.kr>
2025-11-19 17:35:25 +0900

    [Feat] #18 작가 좋아요 기능 구현 완료

--- src/main/java/org/sopt/idus/domain/author/service/AuthorService.java
+++ src/main/java/org/sopt/idus/domain/author/service/AuthorService.java
@@ -5,6 +5,10 @@ import org.sopt.idus.domain.author.dto.response.AuthorResponse;
 import org.sopt.idus.domain.author.entity.Author;
 import org.sopt.idus.domain.author.errorcode.AuthorErrorCode;
 import org.sopt.idus.domain.author.repository.AuthorRepository;
+import org.sopt.idus.domain.authorlike.entity.AuthorLike;
+import org.sopt.idus.domain.authorlike.repository.AuthorLikeRepository;
+import org.sopt.idus.domain.user.entity.User;
+import org.sopt.idus.domain.user.service.UserService;
 import org.sopt.idus.global.exception.customexception.CustomException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +19,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class AuthorService {
 
     private final AuthorRepository authorRepository;
+    private final AuthorLikeRepository authorLikeRepository;
+    private final UserService userService;
 
     public AuthorResponse getAuthorDetail(Long authorId) {
         Author author = findById(authorId);
@@ -22,8 +28,30 @@ public class AuthorService {
         return AuthorResponse.from(author);
     }
 
+    @Transactional
+    public void createAuthorLike(Long authorId, Long userId) {
+        Author author = findById(authorId);
+        User user = userService.findById(userId);
+
+        if(deleteLikeIfPresent(authorId, userId, author)) return;
+        AuthorLike authorLike = AuthorLike.create(author, user);
+        authorLikeRepository.save(authorLike);
+        authorRepository.increaseLikeCount(author);
+    }
+
+    private boolean deleteLikeIfPresent(Long authorId, Long userId, Author author) {
+        if(authorLikeRepository.existsByAuthorIdAndUserId(authorId, userId)) {
+            authorLikeRepository.deleteByAuthorIdAndUserId(authorId, userId);
+            authorRepository.decreaseLikeCount(author);
+            return true;
+        }
+        return false;
+    }
+
     private Author findById(Long authorId) {
         return authorRepository.findById(authorId)
                 .orElseThrow(() -> new CustomException(AuthorErrorCode.AUTHOR_NOT_FOUND));
     }
+
+
 }
