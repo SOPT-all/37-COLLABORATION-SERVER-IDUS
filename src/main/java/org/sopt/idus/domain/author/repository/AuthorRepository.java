b3a332ee30d9788b8dda79e526b36ddbe4756001
dietken1 <dietken1@ajou.ac.kr>
2025-11-19 17:22:50 +0900

    [Feat] #18 작가 좋아요 증감로직 추가

--- src/main/java/org/sopt/idus/domain/author/repository/AuthorRepository.java
+++ src/main/java/org/sopt/idus/domain/author/repository/AuthorRepository.java
@@ -2,7 +2,15 @@ package org.sopt.idus.domain.author.repository;
 
 import org.sopt.idus.domain.author.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    @Modifying
+    @Query("update Author a set a.likeCount = a.likeCount + 1 where a = :author")
+    void increaseLikeCount(Author author);
 
+    @Modifying
+    @Query("update Author a set a.likeCount = a.likeCount - 1 where a = :author")
+    void decreaseLikeCount(Author author);
 }
