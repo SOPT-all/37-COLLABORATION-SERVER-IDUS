942b3702c5f53c11d297946126ead7125d540774
dietken1 <dietken1@ajou.ac.kr>
2025-11-19 14:10:12 +0900

    [Feat] #16 Author Service 구현

--- src/main/java/org/sopt/idus/domain/author/service/AuthorService.java
+++ src/main/java/org/sopt/idus/domain/author/service/AuthorService.java
@@ -1,7 +1,11 @@
 package org.sopt.idus.domain.author.service;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.idus.domain.author.dto.response.AuthorResponse;
+import org.sopt.idus.domain.author.entity.Author;
+import org.sopt.idus.domain.author.errorcode.AuthorErrorCode;
 import org.sopt.idus.domain.author.repository.AuthorRepository;
+import org.sopt.idus.global.exception.customexception.CustomException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,4 +14,15 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class AuthorService {
     private final AuthorRepository authorRepository;
+
+    public AuthorResponse getAuthorDetail(Long authorId) {
+        Author author = findById(authorId);
+
+        return AuthorResponse.from(author);
+    }
+
+    private Author findById(Long authorId) {
+        return authorRepository.findById(authorId)
+                .orElseThrow(() -> new CustomException(AuthorErrorCode.AUTHOR_NOT_FOUND));
+    }
 }
